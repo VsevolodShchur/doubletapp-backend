04e39c856f20da8e3f632dcf8839a09a7da52aa3
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2021-07-06 16:00:34 +0500

    refactoring authentication.py

--- pets/authentication.py
+++ pets/authentication.py
@@ -6,9 +6,8 @@ from pets import settings
 class ApiKeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         x_api_key = request.META.get('HTTP_X_API_KEY')
-        if not x_api_key or x_api_key != settings.API_KEY:
+        if x_api_key != settings.API_KEY:
             raise exceptions.AuthenticationFailed('Invalid Api Key')
-        return None
 
     def authenticate_header(self, request):
         return 'Api Key Authentication'
